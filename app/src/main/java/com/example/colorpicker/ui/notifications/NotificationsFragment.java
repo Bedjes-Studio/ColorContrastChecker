6a67faf27d6308797cad0d5eff0b0975b8bce464
Hugo Langlais <bedjestudio@gmail.com>
2022-12-05 01:04:39 -0500

    Update default state image fragment

--- app/src/main/java/com/example/colorpicker/ui/notifications/NotificationsFragment.java
+++ app/src/main/java/com/example/colorpicker/ui/notifications/NotificationsFragment.java
@@ -7,6 +7,7 @@ import android.content.ClipboardManager;
 import android.content.Context;
 import android.content.Intent;
 import android.graphics.Bitmap;
+import android.graphics.drawable.BitmapDrawable;
 import android.net.Uri;
 import android.os.Bundle;
 import android.provider.MediaStore;
@@ -53,6 +54,7 @@ public class NotificationsFragment extends Fragment {
         binding = FragmentNotificationsBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
 
+
         imageView = root.findViewById(R.id.image1);
         Intent galleryIntent = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
         imageView.setOnClickListener(new View.OnClickListener() {
@@ -83,6 +85,7 @@ public class NotificationsFragment extends Fragment {
         mainColorButton3.setOnClickListener(clipboardCopy);
         mainColorButton4.setOnClickListener(clipboardCopy);
 
+        extractColorsFromBitmap(((BitmapDrawable) imageView.getDrawable()).getBitmap());
         return root;
     }
 
@@ -132,62 +135,4 @@ public class NotificationsFragment extends Fragment {
         button.setText(String.format("#%06X", (0xFFFFFF & swatch.getRgb())));
         button.setTextColor(swatch.getTitleTextColor());
     }
-
-
-    public String getEncoded64ImageStringFromBitmap(Bitmap bitmap) {
-        ByteArrayOutputStream stream = new ByteArrayOutputStream();
-        bitmap.compress(Bitmap.CompressFormat.JPEG, 70, stream);
-        byte[] byteFormat = stream.toByteArray();
-        // get the base 64 string
-        String imgString = Base64.encodeToString(byteFormat, Base64.NO_WRAP);
-        Log.i("ImageToBase62", imgString);
-        return imgString;
-    }
-
-   /* public static int getDominantColor(Bitmap bitmap) {
-        Bitmap newBitmap = Bitmap.createScaledBitmap(bitmap, 275, 231, true);
-        final int color = newBitmap.getPixel(0, 0);
-        newBitmap.recycle();
-        System.out.println(color);
-        return color;
-    }*/
-
-    /*
-    public static int getDominantColor(Bitmap bitmap) {
-        if (bitmap == null) {
-            return Color.TRANSPARENT;
-        }
-        int width = bitmap.getWidth();
-        int height = bitmap.getHeight();
-        int size = width * height;
-        int pixels[] = new int[size];
-        //Bitmap bitmap2 = bitmap.copy(Bitmap.Config.ARGB_4444, false);
-        bitmap.getPixels(pixels, 0, width, 0, 0, width, height);
-        int color;
-        int r = 0;
-        int g = 0;
-        int b = 0;
-        int a;
-        int count = 0;
-        for (int i = 0; i < pixels.length; i++) {
-            color = pixels[i];
-            a = Color.alpha(color);
-            if (a > 0) {
-                r += Color.red(color);
-                g += Color.green(color);
-                b += Color.blue(color);
-                count++;
-            }
-        }
-        r /= count;
-        g /= count;
-        b /= count;
-        r = (r << 16) & 0x00FF0000;
-        g = (g << 8) & 0x0000FF00;
-        b = b & 0x000000FF;
-        color = 0xFF000000 | r | g | b;
-
-        System.out.println(color);
-        return color;
-    }*/
 }
